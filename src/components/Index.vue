5939c4cfaa3aa868fcb84a4a982d983464aba3f5
ququ0129 <50802874+ququ0129@users.noreply.github.com>
2021-04-29 23:58:27 +0800

    完成项目

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,37 +1,60 @@
 <template>
-  <div name="Index">
-    <h1>雾霾探测系统</h1>
-    <p>根据当前ip获取定位：{{ location }}</p>
-    <div class="box">
-      <h2>天气情况</h2>
-      <ul>
-        <li>温度:{{ temp }}</li>
-        <li>体感温度:{{ feelsLike }}</li>
-        <li>天气情况:{{ text }}</li>
-        <li>风向:{{ windDir }}</li>
-        <li>风力:{{ windScale }}</li>
-        <li>风速:{{ windSpeed }}</li>
-        <li>相对湿度:{{ humidity }}</li>
-        <li>能见度：{{ vis }}</li>
-        <li>云量:{{ cloud }}</li>
-      </ul>
-    </div>
-    <div class="box">
-      <h2>空气质量</h2>
-      <ul>
-        <li>质量指数:{{ aqi }}</li>
-        <li>指数等级:{{ level }}</li>
-        <li>指数级别:{{ category }}</li>
-        <li>主要污染物:{{ primary }}</li>
-        <li>PM10:{{ pm10 }}</li>
-        <li>PM2.5:{{ pm2p5 }}</li>
-        <li>二氧化氮:{{ no2 }}</li>
-        <li>二氧化硫:{{ so2 }}</li>
-        <li>一氧化碳:{{ co }}</li>
-        <li>臭氧:{{ o3 }}</li>
-      </ul>
-    </div>
-  </div>
+  <el-container>
+    <el-header style="text-align: left; font-size: 50px; margin-left: 15%">
+      雾霾探测系统示例
+    </el-header>
+    <el-divider></el-divider>
+    <el-main style="margin-left: 15%; margin-right: 15%">
+      <el-row>
+        <el-card class="box-card">
+          <div align="left">
+            <span>根据当前ip获取位置: {{ location }}</span>
+          </div>
+        </el-card>
+      </el-row>
+      <el-row>
+        <el-card class="box-card">
+          <div slot="header" class="clearfix" align="left">
+            <span>天气情况</span>
+          </div>
+          <div>
+            <el-table :data="tableDataWeather" style="width: 100%">
+              <el-table-column prop="temp" label="温度"></el-table-column>
+              <el-table-column prop="text" label="天气情况"> </el-table-column>
+              <el-table-column prop="windDir" label="风向"> </el-table-column>
+              <el-table-column prop="windScale" label="风力"> </el-table-column>
+              <el-table-column prop="windSpeed" label="风速"> </el-table-column>
+              <el-table-column prop="humidity" label="相对湿度">
+              </el-table-column>
+              <el-table-column prop="vis" label="能见度"> </el-table-column>
+              <el-table-column prop="cloud" label="云量"> </el-table-column>
+            </el-table>
+          </div>
+        </el-card>
+      </el-row>
+      <el-row>
+        <el-card class="box-card">
+          <div slot="header" class="clearfix" align="left">
+            <span>空气质量</span>
+          </div>
+          <div>
+            <el-table :data="tableDataAir" style="width: 100%">
+              <el-table-column prop="aqi" label="质量指数"> </el-table-column>
+              <el-table-column prop="level" label="指数等级"> </el-table-column>
+              <el-table-column prop="category" label="指数级别">
+              </el-table-column>
+              <el-table-column prop="primary" label="主要污染物">
+              </el-table-column>
+              <el-table-column prop="pm10" label="PM10"> </el-table-column>
+              <el-table-column prop="pm2p5" label="PM2.5"> </el-table-column>
+              <el-table-column prop="no2" label="二氧化氮"> </el-table-column>
+              <el-table-column prop="so2" label="二氧化硫"> </el-table-column>
+            </el-table>
+          </div>
+        </el-card>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
@@ -39,41 +62,48 @@
 import { getLocation } from "@/request/api";
 import { getWeatherCondition } from "@/request/api";
 import { getAirCondition } from "@/request/api";
+import { getWeatherHourly } from "@/request/api";
+
 export default {
   name: "Index",
   data() {
     return {
-      location: "",
-      temp: "",
-      feelsLike: "",
-      text: "",
-      windDir: "",
-      windScale: "",
-      windSpeed: "",
-      humidity: "",
-      vis: "",
-      cloud: "",
-      aqi: "",
-      level: "",
-      category: "",
-      primary: "",
-      pm10: "",
-      pm2p5: "",
-      no2: "",
-      so2: "",
-      co: "",
-      o3: "",
+      location: localStorage.location,
+      tableDataWeather: [
+        {
+          temp: localStorage.temp,
+          text: localStorage.text,
+          windDir: localStorage.windDir,
+          windScale: localStorage.windScale,
+          windSpeed: localStorage.windSpeed,
+          humidity: localStorage.humidity,
+          vis: localStorage.vis,
+          cloud: localStorage.cloud,
+        },
+      ],
+      tableDataAir: [
+        {
+          aqi: localStorage.aqi,
+          level: localStorage.level,
+          category: localStorage.category,
+          primary: localStorage.primary,
+          pm10: localStorage.pm10,
+          pm2p5: localStorage.pm2p5,
+          no2: localStorage.no2,
+          so2: localStorage.so2,
+        },
+      ],
     };
   },
   mounted: function () {
-    this.getCurrentLocation();
-    this.getCurrentWeather();
-    this.getCurrentAir();
+    setInterval(this.getCurrentLocation, 60 * 30 * 1000);
+    setInterval(this.getCurrentWeather, 60 * 30 * 1000);
+    setInterval(this.getCurrentAir, 60 * 30 * 1000);
   },
   methods: {
     getCurrentLocation: function () {
       getLocation({ lang: "zh-CN" }).then((response) => {
-        this.location = response.city;
+        localStorage.location = response.city;
         localStorage.lat = response.lat;
         localStorage.lon = response.lon;
       });
@@ -84,15 +114,14 @@ export default {
         key: "5c91cb9403384ca88f859860eee30b1e",
       };
       getWeatherCondition(data).then((response) => {
-        this.temp = response.now.temp;
-        this.feelsLike = response.now.feelsLike;
-        this.text = response.now.text;
-        this.windDir = response.now.windDir;
-        this.windScale = response.now.windScale;
-        this.windSpeed = response.now.windSpeed;
-        this.humidity = response.now.humidity;
-        this.vis = response.now.vis;
-        this.cloud = response.now.cloud;
+        localStorage.temp = response.now.temp;
+        localStorage.text = response.now.text;
+        localStorage.windDir = response.now.windDir;
+        localStorage.windScale = response.now.windScale;
+        localStorage.windSpeed = response.now.windSpeed;
+        localStorage.humidity = response.now.humidity;
+        localStorage.vis = response.now.vis;
+        localStorage.cloud = response.now.cloud;
       });
     },
     getCurrentAir: function () {
@@ -101,60 +130,22 @@ export default {
         key: "5c91cb9403384ca88f859860eee30b1e",
       };
       getAirCondition(data).then((response) => {
-        this.aqi = response.now.aqi;
-        this.level = response.now.level;
-        this.category = response.now.category;
-        this.primary = response.now.primary;
-        this.pm10 = response.now.pm10;
-        this.pm2p5 = response.now.pm2p5;
-        this.no2 = response.now.no2;
-        this.so2 = response.now.so2;
-        this.co = response.now.co;
-        this.o3 = response.now.o3;
+        localStorage.aqi = response.now.aqi;
+        localStorage.level = response.now.level;
+        localStorage.category = response.now.category;
+        localStorage.primary = response.now.primary;
+        localStorage.pm10 = response.now.pm10;
+        localStorage.pm2p5 = response.now.pm2p5;
+        localStorage.no2 = response.now.no2;
+        localStorage.so2 = response.now.so2;
       });
     },
   },
 };
 </script>
 
-
-
 <style>
-h1 {
-  text-align: justify;
-  padding-left: 20%;
-}
-p {
-  text-align: justify;
-  padding-top: 1%;
-  padding-left: 20%;
-}
-h2 {
-  text-align: justify;
-  padding-bottom: 5%;
-}
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-}
-ul li {
-  text-align: left;
-}
-* {
-  box-sizing: border-box;
-}
-
-.box {
-  float: left;
-  width: 50%;
-  padding-top: 5%;
-  padding-left: 20%;
-}
-
-.clearfix::after {
-  content: "";
-  clear: both;
-  display: table;
+.el-row {
+  margin-bottom: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
